cbd1e85b6e876702a97b54f788fdabfb5211cb00
evgen <gorkoune@gmail.com>
2023-05-15 20:33:20 +0300

    upload 4 practice

--- workmanager/src/main/java/com/example/workmanager/MainActivity.java
+++ workmanager/src/main/java/com/example/workmanager/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.workmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.Constraints;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WorkRequest uploadWorkRequest =
+                new OneTimeWorkRequest.Builder(UploadWorker.class)
+                        .build();
+        WorkManager
+                .getInstance(this)
+                .enqueue(uploadWorkRequest);
+    }
+    public void onClickButton(View view) {
+        Constraints constraints = new Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.UNMETERED)
+                .setRequiresCharging(true)
+                .build();
+        WorkRequest uploadWorkRequest =
+                new OneTimeWorkRequest.Builder(UploadWorker.class)
+                        .setConstraints(constraints)
+                        .build();
     }
 }
